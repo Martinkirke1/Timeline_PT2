c4b630e222c197a554fb3f24bd12efd8e80b0cc2
Martinkirke1 <martinkirke1@gmail.com>
2016-10-31 09:28:29 -0600

    Initial Commit

---
+++ Timeline_PT2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timeline_PT2
+//
+//  Created by Martin Kirke on 10/31/16.
+//  Copyright © 2016 Ghost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
